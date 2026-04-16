6b441c4b4acb1444bf8976822eac7d8681c8737a
DiegoMicali <diego.micali13@gmail.com>
2026-04-16 08:54:06 -0300

    feat: add create and list stadiums endpoints

---
+++ src/main/java/dev/java10x/elifoot/Mapper/StadiumMapper.java
@@ -0,0 +1,28 @@
+package dev.java10x.elifoot.Mapper;
+
+
+import dev.java10x.elifoot.controller.request.CreateStadiumRequest;
+import dev.java10x.elifoot.controller.response.StadiumResponse;
+import dev.java10x.elifoot.entity.Stadium;
+
+public class StadiumMapper {
+
+    public static StadiumResponse toStadiumResponse (Stadium stadium) {
+        return StadiumResponse.builder()
+                .id(stadium.getId())
+                .name(stadium.getName())
+                .city(stadium.getCity())
+                .capacity(stadium.getCapacity())
+                .urlImg(stadium.getUrlImg())
+                .build();
+    }
+
+    public static Stadium toStadium (CreateStadiumRequest createStadiumRequest) {
+        return Stadium.builder()
+                .name(createStadiumRequest.getName())
+                .city(createStadiumRequest.getCity())
+                .capacity(createStadiumRequest.getCapacity())
+                .urlImg(createStadiumRequest.getUrlImg())
+                .build();
+    }
+}
---
+++ src/main/java/dev/java10x/elifoot/service/CreateStadiumService.java
@@ -0,0 +1,21 @@
+package dev.java10x.elifoot.service;
+
+import dev.java10x.elifoot.Mapper.StadiumMapper;
+import dev.java10x.elifoot.controller.request.CreateStadiumRequest;
+import dev.java10x.elifoot.controller.response.StadiumResponse;
+import dev.java10x.elifoot.entity.Stadium;
+import dev.java10x.elifoot.repository.StadiumRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CreateStadiumService {
+
+    private final StadiumRepository stadiumRepository;
+
+    public StadiumResponse execute (CreateStadiumRequest request) {
+        Stadium savedStadium = stadiumRepository.save(StadiumMapper.toStadium(request));
+        return StadiumMapper.toStadiumResponse(savedStadium);
+    }
+}
